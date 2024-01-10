3bec219e0daeb64fdc9f4939ae8ec79476ead818
Leister Francisco Alvarado Campos <leisterac.1997@gmail.com>
2024-01-10 09:14:42 -0600

    chore(project): add makefile files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-const hello = () => {
-    console.log('Hello world');
-};
-exports.hello = hello;
