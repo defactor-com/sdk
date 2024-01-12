905655e4d01b73fa5452b00269fdbfba08476af4
Leister Francisco Alvarado Campos <leisterac.1997@gmail.com>
2024-01-12 15:27:54 -0600

    feat(assisted-client): add create pool method (#20)

--- src/assisted-client-provider.ts
+++ src/assisted-client-provider.ts
@@ -4,21 +4,40 @@ import { miscErc20CollateralPool } from './artifacts'
 
 type Abi = ethers.Interface | ethers.InterfaceAbi
 
+type Pool = {
+  endTime: number
+  collateralToken: string
+  collateralTokenChainlink: string
+  collateralTokenFactor: number
+  collateralTokenPercentage: number
+  interest: number
+}
+
+type PrivateKey = string | ethers.SigningKey
+
+const DEFAULT_ADMIN_ROLE = '0x' + '00'.repeat(32)
+
 export class AssistedClientProvider {
+  readonly signer: ethers.Wallet
   readonly contract: Contract
   readonly contractAddress: string
   readonly provider: ethers.JsonRpcProvider
   readonly abi: Abi
 
-  constructor(contractAddress: string, apiUrl: string, abi?: Abi) {
+  constructor(
+    contractAddress: string,
+    apiUrl: string,
+    privateKey: PrivateKey,
+    abi?: Abi
+  ) {
     this.contractAddress = contractAddress
     this.provider = new ethers.JsonRpcProvider(apiUrl)
     this.abi = abi || miscErc20CollateralPool.abi
-
+    this.signer = new ethers.Wallet(privateKey, this.provider)
     this.contract = new Contract(
       contractAddress,
       miscErc20CollateralPool.abi,
-      this.provider
+      this.signer
     )
   }
 
@@ -27,5 +46,42 @@ export class AssistedClientProvider {
       await this.contract.LIQUIDATION_PROTOCOL_FEE()
 
     return BigInt(liquidationProtocolFee)
+  }
+
+  async createPool(pool: Pool) {
+    if (!ethers.isAddress(pool.collateralToken)) {
+      throw new Error(
+        'Collateral token does not follow the ethereum address format'
+      )
+    }
+
+    if (!ethers.isAddress(pool.collateralTokenChainlink)) {
+      throw new Error(
+        'Collateral token chainlink does not follow the ethereum address format'
+      )
+    }
+
+    const isAdmin = await this.contract.hasRole(
+      DEFAULT_ADMIN_ROLE,
+      this.signer.address
+    )
+
+    if (!isAdmin) {
+      throw new Error('Sender address is not admin')
+    }
+
+    console.log('Pool ready to be created')
+  }
+
+  async lend() {
+    // code here
+  }
+
+  async borrow() {
+    // code here
+  }
+
+  async repay() {
+    // code here
   }
 }
--- test/unit/assisted-client-provider.test.ts
+++ test/unit/assisted-client-provider.test.ts
@@ -1,11 +1,18 @@
 import { AssistedClientProvider } from '../../src'
-import { loadEnv } from '../test-util'
+import {
+  ADMIN_TESTING_PRIVATE_KEY,
+  COLLATERAL_TOKEN,
+  COLLATERAL_TOKEN_CHAINLINK,
+  INSTANCE_ETH_ADDRESS,
+  TESTING_PRIVATE_KEY,
+  loadEnv
+} from '../test-util'
 
 jest.setTimeout(50000)
 
 describe('AssistedClientProvider', () => {
-  const contractAddress = '0x75154e846dD894C3d2361Cc1d4C03278E0bF38A1'
   let providerUrl: string
+  let contractInstance: AssistedClientProvider
 
   beforeAll(async () => {
     await loadEnv()
@@ -17,14 +24,80 @@ describe('AssistedClientProvider', () => {
     providerUrl = process.env.PROVIDER_URL
   })
 
-  it('get the right liquidation protocol fee', async () => {
-    const contractInstance = new AssistedClientProvider(
-      contractAddress,
-      providerUrl
+  beforeEach(() => {
+    contractInstance = new AssistedClientProvider(
+      INSTANCE_ETH_ADDRESS,
+      providerUrl,
+      TESTING_PRIVATE_KEY
     )
+  })
+
+  it('get the right liquidation protocol fee', async () => {
     const liquidationProtocolFee =
       await contractInstance.LIQUIDATION_PROTOCOL_FEE()
 
     expect(liquidationProtocolFee).toBe(BigInt('5'))
+  })
+
+  it('throws an error if collateralToken is not a valid address', async () => {
+    await expect(
+      contractInstance.createPool({
+        collateralToken: 'invalid',
+        collateralTokenChainlink: COLLATERAL_TOKEN_CHAINLINK,
+        collateralTokenFactor: 10,
+        collateralTokenPercentage: 15,
+        interest: 10,
+        endTime: 1911925999
+      })
+    ).rejects.toThrow(
+      'Collateral token does not follow the ethereum address format'
+    )
+  })
+
+  it('throws an error if collateralTokenChainlink is not a valid address', async () => {
+    await expect(
+      contractInstance.createPool({
+        collateralToken: COLLATERAL_TOKEN,
+        collateralTokenChainlink: 'invalid',
+        collateralTokenFactor: 10,
+        collateralTokenPercentage: 15,
+        interest: 10,
+        endTime: 1911925999
+      })
+    ).rejects.toThrow(
+      'Collateral token chainlink does not follow the ethereum address format'
+    )
+  })
+
+  it('throws an error if sender address is not admin', async () => {
+    await expect(
+      contractInstance.createPool({
+        collateralToken: COLLATERAL_TOKEN,
+        collateralTokenChainlink: COLLATERAL_TOKEN_CHAINLINK,
+        collateralTokenFactor: 10,
+        collateralTokenPercentage: 15,
+        interest: 10,
+        endTime: 1911925999
+      })
+    ).rejects.toThrow('Sender address is not admin')
+  })
+
+  it('logs a message if the pool is ready to be created', async () => {
+    contractInstance = new AssistedClientProvider(
+      INSTANCE_ETH_ADDRESS,
+      providerUrl,
+      ADMIN_TESTING_PRIVATE_KEY
+    )
+
+    await contractInstance.createPool({
+      collateralToken: COLLATERAL_TOKEN,
+      collateralTokenChainlink: COLLATERAL_TOKEN_CHAINLINK,
+      collateralTokenFactor: 10,
+      collateralTokenPercentage: 15,
+      interest: 10,
+      endTime: 1911925999
+    })
+
+    expect(true).toBe(true)
   })
 })
