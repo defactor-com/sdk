e0d3cee7e55ced46816d86d55df4abd54b7bbd58
Torresmorah <torresmorah@gmail.com>
2024-05-09 10:50:34 -0600

    test(pools): add failure test to createPool function

--- test/integration/self-provider-pools.ts
+++ test/integration/self-provider-pools.ts
@@ -1,3 +1,7 @@
+import {
+  cppErrorMessage,
+  poolCommonErrorMessage
+} from '../../src/error-messages'
 import { Pools } from '../../src/pools'
 import { SelfProvider } from '../../src/self-provider'
 import {
@@ -94,21 +98,84 @@ describe('SelfProvider - Pools', () => {
 
   describe('Functions', () => {
     describe('createPool()', () => {
-      it('success - creates a pool', async () => {
-        // TODO: implement this test when the createPool function is full implemented
-        // await provider.contract.createPool({
-        //   softCap: BigInt(230),
-        //   hardCap: BigInt(300),
-        //   deadline: BigInt(1911925999),
-        //   collateralTokens: [
-        //     {
-        //       contractAddress: USD_TOKEN_ADDRESS,
-        //       amount: BigInt(500),
-        //       id: null
-        //     }
-        //   ]
-        // })
-        // expect(true).toBe(true)
+      it('failure - softCap no positive', async () => {
+        await expect(
+          provider.contract.createPool({
+            softCap: BigInt(-10_000000),
+            hardCap: BigInt(5_000000),
+            deadline: BigInt(1715269435),
+            collateralTokens: []
+          })
+        ).rejects.toThrow(cppErrorMessage.noNegativeSoftCapOrZero)
+      })
+      it('failure - hardCap is less than softCap', async () => {
+        await expect(
+          provider.contract.createPool({
+            softCap: BigInt(10_000000),
+            hardCap: BigInt(5_000000),
+            deadline: BigInt(1715269435),
+            collateralTokens: []
+          })
+        ).rejects.toThrow(cppErrorMessage.softCapMustBeLessThanHardCap)
+      })
+      it('failure - deadline is in the past', async () => {
+        await expect(
+          provider.contract.createPool({
+            softCap: BigInt(1_000000),
+            hardCap: BigInt(5_000000),
+            deadline: BigInt(1715269435),
+            collateralTokens: []
+          })
+        ).rejects.toThrow(cppErrorMessage.deadlineMustBeInFuture)
+      })
+      it('failure - one or more collateral token has invalid addresses', async () => {
+        const timeInFuture = Math.floor(Date.now() / 1000) + 60 * 5
+        await expect(
+          provider.contract.createPool({
+            softCap: BigInt(1_000000),
+            hardCap: BigInt(5_000000),
+            deadline: BigInt(timeInFuture),
+            collateralTokens: [
+              {
+                contractAddress: USD_TOKEN_ADDRESS,
+                amount: BigInt(1_000000),
+                id: null
+              },
+              {
+                contractAddress: '0x7h1541v4l1d3x4mpl30f4n4ddr35537h3r3umn37',
+                amount: BigInt(1_000000),
+                id: null
+              },
+              {
+                contractAddress: '0xInvalidAddress',
+                amount: BigInt(1_000000),
+                id: null
+              }
+            ]
+          })
+        ).rejects.toThrow(poolCommonErrorMessage.wrongAddressFormat)
+      })
+      it('failure - one or more collateral token has invalid amounts', async () => {
+        const timeInFuture = Math.floor(Date.now() / 1000) + 60 * 5
+        await expect(
+          provider.contract.createPool({
+            softCap: BigInt(1_000000),
+            hardCap: BigInt(5_000000),
+            deadline: BigInt(timeInFuture),
+            collateralTokens: [
+              {
+                contractAddress: USD_TOKEN_ADDRESS,
+                amount: BigInt(1_000000),
+                id: null
+              },
+              {
+                contractAddress: USD_TOKEN_ADDRESS,
+                amount: BigInt(-1_000000),
+                id: null
+              }
+            ]
+          })
+        ).rejects.toThrow(poolCommonErrorMessage.noNegativeAmountOrZero)
       })
     })
   })
