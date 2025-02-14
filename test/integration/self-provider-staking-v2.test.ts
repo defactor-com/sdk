4bcf6bd7ddab31f56f3036cb222e62605d063e9b
Torresmorah <torresmorah@gmail.com>
2025-02-14 15:35:16 -0600

    test(staking): add test cases for admin functions of staking v2

--- test/integration/self-provider-staking-v2.test.ts
+++ test/integration/self-provider-staking-v2.test.ts
@@ -4,10 +4,14 @@ import { Erc20 } from '../../src'
 import { commonErrorMessage, stakingErrorMessage } from '../../src/errors'
 import { SelfProvider } from '../../src/provider'
 import { StakingV2 } from '../../src/staking'
+import { Plan } from '../../src/types/staking/v2'
 import {
   ADMIN_TESTING_PRIVATE_KEY,
   AMOY_STAKING_CONTRACT_ADDRESS,
   FACTR_TOKEN_ADDRESS,
+  MAX_BIGINT,
+  ONE_DAY_SEC,
+  TESTING_PRIVATE_KEY,
   getUnixEpochTime,
   loadEnv
 } from '../test-util'
@@ -16,8 +20,35 @@ jest.setTimeout(300000)
 
 describe('SelfProvider - Staking', () => {
   let provider: SelfProvider<StakingV2>
+  let notAdminProvider: SelfProvider<StakingV2>
   let signerAddress: string
   let factrTokenContract: Erc20
+
+  const dummyPlan: Omit<Plan, 'totalStaked' | 'totalUnstaked'> & {
+    initialRatio: bigint
+  } = {
+    apy: BigInt(10),
+    apyAfterUnlock: BigInt(5),
+    lockDuration: BigInt(30 * ONE_DAY_SEC),
+    maxStaked: BigInt(1000 * 1e18),
+    minStakeAmount: BigInt(10 * 1e18),
+    initialRatio: BigInt(5),
+    rewardEndTime: BigInt(1744264800),
+    rewardToken: FACTR_TOKEN_ADDRESS,
+    stakingEndTime: BigInt(1739167200),
+    stakingToken: FACTR_TOKEN_ADDRESS
+  }
+
+  const editPlanInput = {
+    planId: BigInt(0),
+    maxStaked: dummyPlan.maxStaked,
+    minStakeAmount: dummyPlan.minStakeAmount,
+    stakingEndTime: dummyPlan.stakingEndTime,
+    rewardEndTime: dummyPlan.rewardEndTime,
+    lockDuration: dummyPlan.lockDuration,
+    apy: dummyPlan.apy,
+    apyAfterUnlock: dummyPlan.apyAfterUnlock
+  }
 
   beforeAll(async () => {
     await loadEnv()
@@ -37,6 +68,13 @@ describe('SelfProvider - Staking', () => {
       AMOY_STAKING_CONTRACT_ADDRESS,
       process.env.PROVIDER_URL,
       ADMIN_TESTING_PRIVATE_KEY
+    )
+
+    notAdminProvider = new SelfProvider(
+      StakingV2,
+      AMOY_STAKING_CONTRACT_ADDRESS,
+      process.env.PROVIDER_URL,
+      TESTING_PRIVATE_KEY
     )
 
     signerAddress = provider.contract.signer?.address || ''
@@ -60,7 +98,210 @@ describe('SelfProvider - Staking', () => {
     })
   })
 
-  describe('Admin Functions', () => {})
+  describe('Admin Functions', () => {
+    describe('withdraw()', () => {
+      it('failure - the signer is not admin', async () => {
+        const validAddress = notAdminProvider.contract.address
+        const res = notAdminProvider.contract.withdraw(
+          FACTR_TOKEN_ADDRESS,
+          validAddress
+        )
+
+        await expect(res).rejects.toThrow(commonErrorMessage.addressIsNotAdmin)
+      })
+
+      it('failure - invalid token address format', async () => {
+        const validAddress = signerAddress
+        const invalidTokenAddress = 'invalid_address'
+
+        await expect(
+          provider.contract.withdraw(invalidTokenAddress, validAddress)
+        ).rejects.toThrow(commonErrorMessage.wrongAddressFormat)
+      })
+      it('failure - invalid recipient address format', async () => {
+        const invalidAddress = 'invalid_address'
+
+        await expect(
+          provider.contract.withdraw(FACTR_TOKEN_ADDRESS, invalidAddress)
+        ).rejects.toThrow(commonErrorMessage.wrongAddressFormat)
+      })
+      it.skip('success - withdraw successfully', async () => {
+        await expect(
+          provider.contract.withdraw(FACTR_TOKEN_ADDRESS, signerAddress)
+        ).resolves.not.toThrow()
+      })
+    })
+    describe('addPlan()', () => {
+      it('failure - invalid staking address format', async () => {
+        const invalidAddress = 'invalid_address'
+
+        await expect(
+          provider.contract.addPlan({
+            ...dummyPlan,
+            stakingToken: invalidAddress
+          })
+        ).rejects.toThrow(commonErrorMessage.wrongAddressFormat)
+
+        await expect(
+          provider.contract.addPlan({
+            ...dummyPlan,
+            rewardToken: invalidAddress
+          })
+        ).rejects.toThrow(commonErrorMessage.wrongAddressFormat)
+      })
+      it('failure - negative values', async () => {
+        await expect(
+          provider.contract.addPlan({ ...dummyPlan, maxStaked: BigInt(-1) })
+        ).rejects.toThrow(stakingErrorMessage.minStakeMustBeLessThanMaxStake)
+
+        await expect(
+          provider.contract.addPlan({
+            ...dummyPlan,
+            minStakeAmount: BigInt(-1)
+          })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeMinStakeAmount)
+
+        await expect(
+          provider.contract.addPlan({ ...dummyPlan, initialRatio: BigInt(-1) })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeInitialRatio)
+
+        await expect(
+          provider.contract.addPlan({ ...dummyPlan, lockDuration: BigInt(-1) })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeLockDuration)
+
+        await expect(
+          provider.contract.addPlan({ ...dummyPlan, apy: BigInt(-1) })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeApy)
+
+        await expect(
+          provider.contract.addPlan({
+            ...dummyPlan,
+            apyAfterUnlock: BigInt(-1)
+          })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeApy)
+      })
+      it('failure - invalid staking and reward time', async () => {
+        await expect(
+          provider.contract.addPlan({ ...dummyPlan, rewardEndTime: BigInt(0) })
+        ).rejects.toThrow(stakingErrorMessage.rewardEndTimeTooLow)
+      })
+      it('failure - max staked is less than min staked', async () => {
+        await expect(
+          provider.contract.addPlan({
+            ...dummyPlan,
+            minStakeAmount:
+              (dummyPlan.minStakeAmount + dummyPlan.maxStaked) * BigInt(2)
+          })
+        ).rejects.toThrow(stakingErrorMessage.minStakeMustBeLessThanMaxStake)
+      })
+      it.skip('failure - plan already exists', async () => {
+        await expect(provider.contract.addPlan(dummyPlan)).rejects.toThrow(
+          stakingErrorMessage.planAlreadyExists
+        )
+      })
+      it.skip('success - add new plan successfully', async () => {
+        await expect(
+          provider.contract.addPlan(dummyPlan)
+        ).resolves.not.toThrow()
+      })
+    })
+    describe('changeTokenRatioForPlan()', () => {
+      it('failure - negative plan Id', async () => {
+        const planId = BigInt(-1)
+        const newRatio = BigInt(4)
+
+        await expect(
+          provider.contract.changeTokenRatioForPlan(planId, newRatio)
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeIndexId)
+      })
+      it('failure - plan does not exists', async () => {
+        const planId = BigInt(MAX_BIGINT)
+        const newRatio = BigInt(4)
+
+        await expect(
+          provider.contract.changeTokenRatioForPlan(planId, newRatio)
+        ).rejects.toThrow(stakingErrorMessage.invalidPlanId)
+      })
+      it('failure - negative new ratio', async () => {
+        const planId = BigInt(0)
+        const newRatio = BigInt(-1)
+
+        await expect(
+          provider.contract.changeTokenRatioForPlan(planId, newRatio)
+        ).rejects.toThrow(commonErrorMessage.nonNegativeValue)
+      })
+      it.skip('success - add new ratio to the plan', async () => {
+        const planId = BigInt(0)
+        const newRatio = BigInt(4)
+
+        await expect(
+          provider.contract.changeTokenRatioForPlan(planId, newRatio)
+        ).resolves.not.toThrow()
+      })
+    })
+    describe('editPlan()', () => {
+      it('failure - negative values', async () => {
+        await expect(
+          provider.contract.editPlan({ ...editPlanInput, planId: BigInt(-1) })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeIndexId)
+
+        await expect(
+          provider.contract.editPlan({
+            ...editPlanInput,
+            minStakeAmount: BigInt(-1)
+          })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeMinStakeAmount)
+
+        await expect(
+          provider.contract.editPlan({
+            ...editPlanInput,
+            lockDuration: BigInt(-1)
+          })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeLockDuration)
+
+        await expect(
+          provider.contract.editPlan({ ...editPlanInput, apy: BigInt(-1) })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeApy)
+
+        await expect(
+          provider.contract.editPlan({
+            ...editPlanInput,
+            apyAfterUnlock: BigInt(-1)
+          })
+        ).rejects.toThrow(stakingErrorMessage.nonNegativeApy)
+      })
+      it('failure - invalid staking and reward time', async () => {
+        await expect(
+          provider.contract.editPlan({
+            ...editPlanInput,
+            rewardEndTime: BigInt(0)
+          })
+        ).rejects.toThrow(stakingErrorMessage.rewardEndTimeTooLow)
+      })
+      it('failure - max staked is less than min staked', async () => {
+        await expect(
+          provider.contract.editPlan({
+            ...editPlanInput,
+            minStakeAmount:
+              (dummyPlan.minStakeAmount + dummyPlan.maxStaked) * BigInt(2)
+          })
+        ).rejects.toThrow(stakingErrorMessage.minStakeMustBeLessThanMaxStake)
+      })
+      it.skip('failure - plan already exists', async () => {
+        await expect(provider.contract.editPlan(editPlanInput)).rejects.toThrow(
+          stakingErrorMessage.planAlreadyExists
+        )
+      })
+      it.skip('success - edit plan successfully', async () => {
+        await expect(
+          provider.contract.editPlan({
+            ...editPlanInput,
+            minStakeAmount: BigInt(100 * 1e18)
+          })
+        ).resolves.not.toThrow()
+      })
+    })
+  })
 
   describe('Functions', () => {})
 
@@ -111,6 +352,13 @@ describe('SelfProvider - Staking', () => {
           provider.contract.getPlanTokenRatios(planId)
         ).rejects.toThrow(stakingErrorMessage.nonNegativeIndexId)
       })
+      it('failure - plan id is invalid', async () => {
+        const planId = BigInt(MAX_BIGINT)
+
+        await expect(
+          provider.contract.getPlanTokenRatios(planId)
+        ).rejects.toThrow(stakingErrorMessage.invalidPlanId)
+      })
       it('success - get plan token ratios', async () => {
         const planId = BigInt(0)
         const tokenRatios = await provider.contract.getPlanTokenRatios(planId)
@@ -155,7 +403,15 @@ describe('SelfProvider - Staking', () => {
           provider.contract.getUserStake(validAddress, stakeIndex)
         ).rejects.toThrow(stakingErrorMessage.nonNegativeIndexId)
       })
-      it('success - get user stakes', async () => {
+      it('failure - stake index is invalid', async () => {
+        const validAddress = signerAddress
+        const stakeIndex = BigInt(MAX_BIGINT)
+
+        await expect(
+          provider.contract.getUserStake(validAddress, stakeIndex)
+        ).rejects.toThrow(stakingErrorMessage.invalidStakeIndex)
+      })
+      it.skip('success - get user stakes', async () => {
         const validAddress = signerAddress
         const stakeIndex = BigInt(0)
         const stake = await provider.contract.getUserStake(
@@ -207,7 +463,20 @@ describe('SelfProvider - Staking', () => {
           )
         ).rejects.toThrow(stakingErrorMessage.nonNegativeIndexId)
       })
-      it('success - get stake rewards by index', async () => {
+      it('failure - stake index is invalid', async () => {
+        const validAddress = signerAddress
+        const stakeIndex = BigInt(MAX_BIGINT)
+        const timestamp = getUnixEpochTime()
+
+        await expect(
+          provider.contract.calculateStakeRewardByIndex(
+            validAddress,
+            timestamp,
+            stakeIndex
+          )
+        ).rejects.toThrow(stakingErrorMessage.invalidStakeIndex)
+      })
+      it.skip('success - get stake rewards by index', async () => {
         const validAddress = signerAddress
         const stakeIndex = BigInt(0)
         const timestamp = getUnixEpochTime()
